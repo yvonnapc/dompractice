b291b0117ea81c27c37a046add76c29afab2765d
Jason Wuerch and Yvonna Contreras <wuerchjason@gmail.com>
2016-01-12 11:14:49 -0800

    Add image buttons

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     $("#cat").prepend("<li>Meow!</li>");
     $("#dog").prepend("<li>Bark!</li>");
     $("#cat").children("li").first().click(function() {
-      alert("MEOW");
-    $("#dog").children("li").click(function() {
-      alert("BARK");
-
+      $(this).remove();
     });
+    $("#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+
   });
   $("button#goodbye").click(function() {
     $("ul").prepend("<li>Goodbye</li>");
@@ -15,5 +16,17 @@ $(document).ready(function() {
 
   $("#seeyou").click(function() {
     $("ul").prepend("<li>See you later</li>");
-  })
+  });
+
+  $("button#magic").click(function(){
+    $("#img-wrap").css("visibility","hidden")
+
+  });
+
+  $("button#magic2").click(function(){
+    $("#img-wrap").css("visibility","visible")
+
+  });
+
+
 });
